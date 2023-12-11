8843415fe5b3a66368dc5647bac2e31313e1aa96
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-12-11 19:44:59 +0900

    (2023.12.11) Button Event

---
+++ kjwUnityTutorial/Assets/UGUI/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        Debug.Log("New Game");
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game");
+    }
+
+    public void Option()
+    {
+        Debug.Log("Option");
+
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+
+    }
+}

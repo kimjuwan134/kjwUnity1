95afdefd55203e0e770343571203f3b7dd38b750
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-12-14 19:47:30 +0900

    (2023.12.14) Animation Clip

---
+++ kjwUnityTutorial/Assets/Animation Clip/Scripts/AnimatorManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AnimatorManager : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] AnimationClip[] animationClip;
+
+    void Start()
+    {
+        for(int i = 0; i < animationClip.Length; i++)
+        {
+            var data = AnimationUtility.GetAnimationClipSettings(animationClip[i]);
+
+            data.loopTime = false;
+
+            AnimationUtility.SetAnimationClipSettings(animationClip[i], data);
+        }
+    }
+
+    void Update()
+    {
+        // 현재 애니메이션의 이름이 "Close" 라면
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Close"))
+        {
+            // 현재 진행한 애니메이션의 실행 상태를 의미.
+            if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+            {
+                // SetActive : 게임 오브젝트를 활성화 또는 비활성화 할 수 있는 함수.
+                animator.gameObject.SetActive(false);
+            }
+        }
+    }
+}
